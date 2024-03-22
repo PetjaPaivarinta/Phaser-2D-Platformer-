bacd222b5db9e89f17afd93edd99bc5b2af943dc
PetjaPaivarinta <sapors10@gmail.com>
2024-03-22 10:54:36 +0200

    added loading bar

--- Assets/Scripts/World.js
+++ Assets/Scripts/World.js
@@ -8,9 +8,74 @@ class World extends Phaser.Scene
     
     preload()
     {
+        var width = this.cameras.main.width;
+var height = this.cameras.main.height; // Corrected typo
+
+    var progressBar = this.add.graphics();
+    var progressBox = this.add.graphics();
+
+    progressBox.fillRect(width / 2 - 30, height / 2 - 30, 2, 2);
+    var loadingText = this.make.text({
+        x: width / 2,
+        y: height / 2 - 50,
+        text: 'Loading...',
+        style: {
+            font: '22px monospace',
+            fill: '#ddddd'
+        }
+    });
+
+    loadingText.setOrigin(0.5, 0.5);
+    
+    var percentText = this.make.text({
+        x: width / 2,
+        y: height / 2 - 5,
+        text: '0%',
+        style: {
+            font: '22px monospace',
+            fill: '#ddddd'
+        }
+    });
+    percentText.setOrigin(0.5, 0.5);
+    
+    var assetText = this.make.text({
+        x: width / 2,
+        y: height / 2 + 50,
+        text: '',
+        style: {
+            font: '22px monospace',
+            fill: '#ddddd'
+
+        }
+    });
+    assetText.setOrigin(0.5, 0.5);
+    
+    this.load.on('progress', function (value) {
+        percentText.setText(parseInt(value * 100) + '%');
+        progressBar.clear();
+        progressBar.fillStyle(0xffffff, 1);
+        progressBar.fillRect(width / 2 - 160, height / 2 - 30, 320 * value, 50);
+    });
+    
+    this.load.on('fileprogress', function (file) {
+        assetText.setText('Loading asset: ' + file.key);
+    });
+
+   this.load.on('complete', function () {
+        progressBar.destroy();
+        progressBox.destroy();
+        loadingText.destroy();
+        percentText.destroy();
+        assetText.destroy();
+        console.log("dwdwdw")
+    });
         this.load.image('start player', 'Assets/Images/1st Player img.png');
         this.load.image('second player', 'Assets/Images/2nd Player img.png');
         this.load.image('coin', 'Assets/Images/coin.png');
+
+        for (let i = 0; i < 100; i++) {
+            this.load.image('coin' + i, 'Assets/Images/coin.png');
+        }
 
 
         // load tilemap
